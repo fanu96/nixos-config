760acf19285f756b4c319ca6dc7879cced20c510
fanu96 <fanuel.berhanu-ug@aau.edu.et>
2024-11-29 04:46:06 +0300

    Updated

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    wget
+   python2
+   python3
    brave
    python311Packages.adblock
    python312Packages.adblock
